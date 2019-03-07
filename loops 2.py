8eb81445d4a732a1926e6e3960a4f96bbedc2024
Linsi6280 <47427494+Linsi6280@users.noreply.github.com>
2019-03-07 14:29:15 -0500

    Add files via upload

---
+++ loops 2.py
@@ -0,0 +1,20 @@
+# 1
+for i in range(10):
+#f or i in " " * 10:
+    print("hello")
+
+
+# 2
+for i in range(0, 101, 3):
+    print(i)
+
+for num in range(101):
+    if num % 3 == 0:
+        print(num)
+
+# 3
+total = 0
+for num in range(1, 11):
+    total += num
+print(total)
+
